6fdd314f2a5057bf8e1de85877f58582c699b477
Sean Johnson <seanjohnson220@gmail.com>
2021-08-25 11:35:01 -0400

    completing styling and working on dynamic background image functionality

---
+++ script.js
@@ -0,0 +1,21 @@
+// Target the body
+const body = document.querySelector("body");
+
+
+
+const apiKey = "AIzaSyBwK12OiwiuO6MiJFKLQ3KY5LhAjh_DAh0";
+const apiURL = `https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=600x300&maptype=roadmap
+&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318
+&markers=color:red%7Clabel:C%7C40.718217,-73.998284
+&key=`
+
+async function fetchBackgroundImage() {
+    const response = await fetch(apiURL + apiKey)
+    const responseJson = response.json();
+
+    // body.setAttribute("style", "background: url(" + responseJson +");background-repeat: no=repeat")
+    const myStyle = body.getAttribute("style");
+    console.log(myStyle)
+}
+
+fetchBackgroundImage();
