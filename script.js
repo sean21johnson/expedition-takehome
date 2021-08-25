02096af07300dc3f275a779e26a7bd3f40b8b764
Sean Johnson <seanjohnson220@gmail.com>
2021-08-25 15:31:58 -0400

    functionality and styling complete

--- script.js
+++
@@ -1,8 +0,0 @@
-// Target the body
-const body = document.querySelector("body");
-
-const apiKey = "AIzaSyBwK12OiwiuO6MiJFKLQ3KY5LhAjh_DAh0";
-const apiURL = `https://maps.googleapis.com/maps/api/staticmap?center=15714+Melrose+Ave,+Beverly+Hills,+CA+90201&zoom=13&scale=1&size=600x300&maptype=roadmap&key=AIzaSyBwK12OiwiuO6MiJFKLQ3KY5LhAjh_DAh0&format=png&visual_refresh=true`;
-
-
-console.log(apiURL + apiKey)
