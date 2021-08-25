03b1ee920501f05dda900b34df3314eb58ca2d4e
Sean Johnson <seanjohnson220@gmail.com>
2021-08-25 12:51:20 -0400

    updated

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const body = document.querySelector("body");
 
 
 
-const apiKey = "AIzaSyBwK12OiwiuO6MiJFKLQ3KY5LhAjh_DAh0";
+
 const apiURL = `https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=600x300&maptype=roadmap
 &markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318
 &markers=color:red%7Clabel:C%7C40.718217,-73.998284
